b014b6d36adfc053d5b33063077547a0cdd68931
Andy Ko <peanut61313@gmail.com>
2018-08-22 19:59:01 -0500

    initial push

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -2,4 +2,7 @@
 
 document.addEventListener("DOMContentLoaded", function(event) {
   // You code here
+  var length = list[0].children.length;
+  window.alert("there is" + length);
 });
+console.log("hello");
