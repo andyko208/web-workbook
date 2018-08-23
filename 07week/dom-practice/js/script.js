d12117529e0713a9fa8027c19aafe8bb301b0dbb
Andy Ko <peanut61313@gmail.com>
2018-08-22 22:49:54 -0500

    second push

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,8 +1,30 @@
 'use strict';
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  // You code here
-  var length = list[0].children.length;
-  window.alert("there is" + length);
-});
-console.log("hello");
+  window.onload = function(){
+    // listCount();
+    // itemAlert();
+    // insertItemCounter();
+    // insertAddItem();
+    // insertRemoveItemExistingItems();
+    // var counts = document.getElementsByTagName('ul');
+    // console.log(counts[0].children.length);
+    var count = document.querySelectorAll('li')
+      console.log(count.length);
+
+      document.querySelector('h1').insertAdjacentHTML("afterEnd",
+    "<h2> You have " + count.length + " items in your shopping cart </h2>");
+  }
+  // function listCount() {
+  //   let list = document.getElementByTagName("ul");
+  //   return list[0].children.length;
+  // }
+  // function itemAlert() {
+  //   let list = document.getElementByTagName("ul");
+  //   alert("there are " + listCount() + "items in your shopping cart");
+  // }
+  // function insertAddItem() {
+  //   var newInput = document.createElement('input');
+  //   newInput.setAttribute("placeholder", "Type to add");
+  //   let cart = document.getElementByTagName("ul");
+  //   cart[0].after(newInput);
+  //   newInput.setAttribute("id", "input-text")
