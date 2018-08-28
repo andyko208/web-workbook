d43090c61215b73c319c31b7af36039c5e40a951
Andy Ko <peanut61313@gmail.com>
2018-08-28 18:05:54 -0500

    buttons

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -8,23 +8,35 @@
     // insertRemoveItemExistingItems();
     // var counts = document.getElementsByTagName('ul');
     // console.log(counts[0].children.length);
-    var count = document.querySelectorAll('li')
+    var count = document.querySelectorAll('li');
       console.log(count.length);
-
       document.querySelector('h1').insertAdjacentHTML("afterEnd",
     "<h2> You have " + count.length + " items in your shopping cart </h2>");
+    alert("There are " + count.length + " items in your shopping cart!");
+    insertAddItem();
   }
-  // function listCount() {
-  //   let list = document.getElementByTagName("ul");
-  //   return list[0].children.length;
+  // add list item
+  // function changeText1() {
+  //   var element = document.getElementById('addl');
+  //   var cont = document.createElement('li');
+  //   cont.appendChild(document.createTextNode("placeholder"));
+  //   element.appendChild(cont);
   // }
-  // function itemAlert() {
-  //   let list = document.getElementByTagName("ul");
-  //   alert("there are " + listCount() + "items in your shopping cart");
+  // // delete last item
+  // function changeText2() {
+  //   var element = document.getElementById('addl');
+  //   var last = element.lastChild;
+  //   element.removeChild(last);
   // }
-  // function insertAddItem() {
-  //   var newInput = document.createElement('input');
-  //   newInput.setAttribute("placeholder", "Type to add");
-  //   let cart = document.getElementByTagName("ul");
-  //   cart[0].after(newInput);
-  //   newInput.setAttribute("id", "input-text")
+// button.onclick = methodName;
+  function insertAddItem() {
+    var newInput = document.createElement('input');
+    newInput.setAttribute("placeholder", "Type to add");
+    let cart = document.getElementByTagName("ul");
+    cart[0].after(newInput);
+    newInput.setAttribute("id", "input-text")
+  }
+  function AddItem() {
+    var button = document.createElement('input');
+    button.setAttribute("input")
+  }
