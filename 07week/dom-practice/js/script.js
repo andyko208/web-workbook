c2735ced040d6e2ce50a27e7fabbcf53f8ebaf58
Andy Ko <peanut61313@gmail.com>
2018-08-29 19:37:24 -0500

    button functions not yet

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -12,8 +12,10 @@
       console.log(count.length);
       document.querySelector('h1').insertAdjacentHTML("afterEnd",
     "<h2> You have " + count.length + " items in your shopping cart </h2>");
-    alert("There are " + count.length + " items in your shopping cart!");
+    alert('You have ' + count.length + ' items in your shopping cart!');
     insertAddItem();
+    AddItem();
+    DeleteItem();
   }
   // add list item
   // function changeText1() {
@@ -32,11 +34,40 @@
   function insertAddItem() {
     var newInput = document.createElement('input');
     newInput.setAttribute("placeholder", "Type to add");
-    let cart = document.getElementByTagName("ul");
+    let cart = document.getElementsByTagName('ul');
     cart[0].after(newInput);
     newInput.setAttribute("id", "input-text")
   }
   function AddItem() {
     var button = document.createElement('input');
-    button.setAttribute("input")
+    button.setAttribute("type", "button");
+    button.setAttribute("value", "Add");
+    button.setAttribute("id", "add");
+    let cart = document.getElementById('input-text');
+    cart.after(button);
+    var a = document.getElementById('input-text').value;
+    button.onclick = function(event){
+      create();
+    }
+  }
+  function DeleteItem() {
+    var button2 = document.createElement('input');
+    button2.setAttribute("type", "button");
+    button2.setAttribute("value", "Delete");
+    button2.setAttribute("id", "delete");
+    let cart = document.getElementById('add');
+    cart.after(button2);
+    button2.onclick = function(event){
+      deletea();
+    }
+  }
+    // button2.onclick = document.removeChild(document.getElementsByTagName('ul').lastChild);
+  function create() {
+    let ls = document.createElement('li');
+    let cart = document.getElementsByTagName('ul');
+    cart[0].appendChild(ls);
+  }
+  function deletea() {
+    var dl = document.getElementsByTagName('ul');
+    dl.removeChild(dl.lastChild);
   }
