9490b357d40685dfc35ad54ae7b6e6ccb88913b0
Andy Ko <peanut61313@gmail.com>
2018-08-29 22:48:37 -0500

    initial push

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,5 +1,30 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic in here
+  var turn = 'O';
+  $('[data-cell]').on('click', function(){
+    if ($(this).text() === ''){
+      // checkwin();
+      $(this).text(turn);
+      if(turn === 'O'){
+        turn = 'X';
+      }
+      else{
+        turn = 'O';
+      }
+    }
+
+  })
 });
+// function checkwin() {
+//   if($('data-cell = "0"').text() === turn &&
+//       $('data-cell = "1"').text() === turn &&
+//       $('data-cell = "2"').text() === turn)
+//     $('#announce-winner').text('X wins');
+// }
+//
+// $('#clear'.click(function(){
+//   $('[data-cell]').empty();
+//   turn = "X";
+//   $('#announce-winner').empty();
+// })
