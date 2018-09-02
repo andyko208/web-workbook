68ef2a192e146f62f9f819dd6b9a1cd51707330f
Andy Ko <peanut61313@gmail.com>
2018-09-02 13:22:06 -0500

    final

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,42 +1,21 @@
 'use strict';
 
   window.onload = function(){
-    // listCount();
-    // itemAlert();
-    // insertItemCounter();
-    // insertAddItem();
-    // insertRemoveItemExistingItems();
-    // var counts = document.getElementsByTagName('ul');
-    // console.log(counts[0].children.length);
-    var count = document.querySelectorAll('li');
-      console.log(count.length);
-      document.querySelector('h1').insertAdjacentHTML("afterEnd",
-    "<h2> You have " + count.length + " items in your shopping cart </h2>");
-    alert('You have ' + count.length + ' items in your shopping cart!');
     insertAddItem();
     AddItem();
     DeleteItem();
+    var count = document.querySelectorAll('li');
+    console.log(count.length);
+    document.querySelector('h1').insertAdjacentHTML("afterEnd",
+    "<h2> You have " + count.length + " items in your shopping cart </h2>");
+    alert('You have ' + count.length + ' items in your shopping cart!');
   }
-  // add list item
-  // function changeText1() {
-  //   var element = document.getElementById('addl');
-  //   var cont = document.createElement('li');
-  //   cont.appendChild(document.createTextNode("placeholder"));
-  //   element.appendChild(cont);
-  // }
-  // // delete last item
-  // function changeText2() {
-  //   var element = document.getElementById('addl');
-  //   var last = element.lastChild;
-  //   element.removeChild(last);
-  // }
-// button.onclick = methodName;
   function insertAddItem() {
     var newInput = document.createElement('input');
     newInput.setAttribute("placeholder", "Type to add");
     let cart = document.getElementsByTagName('ul');
     cart[0].after(newInput);
-    newInput.setAttribute("id", "input-text")
+    newInput.setAttribute("id", "input-text");
   }
   function AddItem() {
     var button = document.createElement('input');
@@ -45,7 +24,6 @@
     button.setAttribute("id", "add");
     let cart = document.getElementById('input-text');
     cart.after(button);
-    var a = document.getElementById('input-text').value;
     button.onclick = function(event){
       create();
     }
@@ -61,13 +39,24 @@
       deletea();
     }
   }
-    // button2.onclick = document.removeChild(document.getElementsByTagName('ul').lastChild);
   function create() {
-    let ls = document.createElement('li');
-    let cart = document.getElementsByTagName('ul');
-    cart[0].appendChild(ls);
+    let ad = document.getElementById('input-text').value;
+    document.querySelector('ul').insertAdjacentHTML("beforeend",
+    "<li>" + ad + "</li>")
+    let count = document.querySelectorAll('li');
+    console.log(count.length);
+    document.querySelector('h1').insertAdjacentHTML("afterEnd",
+    "<h2> You have " + count.length + " items in your shopping cart </h2>");
+    let key = document.getElementById('key');
+    key.removeChild(key.childNodes[3]);
   }
   function deletea() {
-    var dl = document.getElementsByTagName('ul');
+    var dl = document.getElementById('dec');
     dl.removeChild(dl.lastChild);
+    let count = document.querySelectorAll('li');
+    console.log(count.length);
+    document.querySelector('h1').insertAdjacentHTML("afterEnd",
+    "<h2> You have " + count.length + " items in your shopping cart </h2>");
+    let key = document.getElementById('key');
+    key.removeChild(key.childNodes[3]);
   }
