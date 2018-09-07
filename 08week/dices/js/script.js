2478e4fbfbf254aa314d0985fd845366a0ac6424
Andy Ko <peanut61313@gmail.com>
2018-09-07 14:48:31 -0500

    straights left

--- 08week/dices/js/script.js
+++ 08week/dices/js/script.js
@@ -18,28 +18,25 @@ roll();
 function report() {
   document.querySelector("#report").innerHTML = ""; // clear out the report box
   let diceHtml = document.querySelectorAll('.die'); // this puts all the individual die HTML elements in diceHtml
+  let doub = 0;     //two-pair
+  let trip = 0;     //full-house
+  let count = 0;
+  let ss = 1;
   for (let dieVal = 1; dieVal < 7; dieVal++) {  // we set up a nested loop, first we check for all the 1s, then 2s, etc
     let howManyDice = 0; // we zero out our counter
-    let doub = 0;
-    let trip = 0;
     for (let i = 0; i < 5; i++) {  // we create the inner loop that cycles through the rolled dice
       if (dieVal === Number(diceHtml[i].getAttribute('data-roll'))) {  // we check if the rolled die is equal to the one's we're counting
         // console.log(Number(diceHtml[i].getAttribute('data-roll')));
         howManyDice++; // if so, we add one to the count
-        if (howManyDice === 2){
-          doub+= 1;
-          console.log("this is # of doubles " + doub);
-        }
-        if (doub === 2) {
-          document.querySelector("#report").innerHTML += `There is a two-pair<br>`;
-        }
       }
     }
     if (howManyDice === 2) { // we check after the counting is done if it is equal to '2'
       document.querySelector("#report").innerHTML += `There are a pair of ${dieVal}s<br>`;  // if yes, we report a pair
+      doub++;
     }
     if (howManyDice === 3) { // we check after the counting is done if it is equal to '3'
       document.querySelector("#report").innerHTML += `There are a three of ${dieVal}s<br>`;  // if yes, we report a three
+      trip++;
     }
     if (howManyDice === 4) { // we check after the counting is done if it is equal to '4'
       document.querySelector("#report").innerHTML += `There are a four of ${dieVal}s<br>`;  // if yes, we report a four
@@ -48,9 +45,21 @@ function report() {
       document.querySelector("#report").innerHTML += `There are a yacht of ${dieVal}s<br>`;  // if yes, we report a yacht
     }
   }
+  if (doub === 2) {
+    document.querySelector("#report").innerHTML += `There is a two-pair<br>`;
+  }
+  if (doub === 1 && trip === 1) {
+    document.querySelector("#report").innerHTML += `There is a full-house<br>`;
+  }
+  for (let i = 0; i < 5 ; i++) {
+    if (ss === Number(diceHtml[i].getAttribute('data-roll'))) {
+      count++;
+      ss++;
+      console.log(count + " is count");
+      console.log(ss + " is ss");
+    }
+  }
 
-// two pairs at once, like 3 6 5 3 5 - called two-pairs;
-// pair and three at once, like 1 6 6 1 6 - called full-house;
 // sequence from 1 to 5, like 2 4 3 5 1 - called small-straight;
 // sequence from 2 to 6, like 6 3 4 2 5 - called big-straight.
 }
